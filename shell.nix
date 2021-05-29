bb7d7504c414bec1ab3279c8cf782e9ef66833dd
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2021-05-29 22:11:39 +0300

    Pin nixpkgs, set up publish action

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs ? import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/84aa23742f6c72501f9cc209f29c438766f5352d.tar.gz") { } }:
 
 pkgs.mkShell {
   nativeBuildInputs = with pkgs; [ hugo ];
