659b0fbddd72bd63524392dd2e02c16f5389774a
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2021-12-24 19:26:23 +0200

    Use Nix flakes, update Hugo to 0.91.1

--- shell.nix
+++
@@ -1,5 +0,0 @@
-{ pkgs ? import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/84aa23742f6c72501f9cc209f29c438766f5352d.tar.gz") { } }:
-
-pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [ hugo ];
-}
